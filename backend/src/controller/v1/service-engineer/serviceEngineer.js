bdb9a77e36b59fc3e0079a5e19ace2708b4ddf42
tanjilahamed99 <tanjil113355@gmail.com>
2026-09-23 09:59:40 +0600

    testing

--- backend/src/controller/v1/service-engineer/serviceEngineer.js
+++ backend/src/controller/v1/service-engineer/serviceEngineer.js
@@ -401,7 +401,7 @@ exports.serviceEngineerCloseJob = async (req, res) => {
       });
     }
 
-    res.status(200).json({ success: true, message: "Job closed", data: job });
+    // res.status(200).json({ success: true, message: "Job closed", data: job });
 
     generateAndSendServiceReport(job._id).catch((err) => {
       console.error(
